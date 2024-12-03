e3cbb4b28cf64d13fec416c5fe2c25e28ec04102
xn t <xnt@xndeMacBook-Air.local>
2024-12-03 16:54:23 +0800

    封装并使用CORS跨域中间件

--- internal/route.go
+++ internal/route.go
@@ -5,10 +5,12 @@ import (
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"txnbi-backend/internal/handle"
+	"txnbi-backend/middleware"
 )
 
 func Route() *gin.Engine {
 	routes := gin.Default()
+	routes.Use(middleware.CORSMiddleware())
 	userGroup := routes.Group("/user")
 	{
 		userGroup.POST("/login", handle.UserLogin)       // 用户登陆接口
---
+++ middleware/cors.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	// 配置 CORS 中间件
+	config := cors.Config{
+		AllowOrigins:     []string{"http://localhost:8000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+	return cors.New(config)
+}
