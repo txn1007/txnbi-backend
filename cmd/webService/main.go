b07289a0754749e73a24ed0d9f56c21679c7219f
txn1007 <962782329@qq.com>
2024-12-15 14:36:38 +0800

    重新封装zerolog，支持以json格式向标准错误输出和本地文件写入日志

--- cmd/webService/main.go
+++ cmd/webService/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"github.com/rs/zerolog/log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"txnbi-backend/conf"
 	_ "txnbi-backend/docs"
 	txnbi "txnbi-backend/internal"
+	"txnbi-backend/pkg/tlog"
 )
 
 //	@title			txnbi API
@@ -14,12 +23,40 @@ import (
 //	@BasePath		/
 
 func main() {
+	// 启动服务器
+	// 创建路由并启动服务器
 	r := txnbi.Route()
 	addr := fmt.Sprintf("%s:%d", conf.TxnBICfg.Host, conf.TxnBICfg.Port)
-	err := r.Run(addr)
-	if err != nil {
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+	go func() {
+		log.Info().Msgf("服务器启动，监听端口: %d", conf.TxnBICfg.Port)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal().Err(err).Msg("服务器启动失败")
+		}
+	}()
+
+	// 关闭服务器
+	// 监听信号，当收到信号后进行关停
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	// 创建一个带超时的上下文，尝试优雅地关闭服务器
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal().Err(err).Msg("优雅关停失败")
+	}
+	log.Info().Msg("关闭服务器成功")
+
+	// 关闭日志文件句柄
+	if err := tlog.CloseLogFile(); err != nil {
 		panic(err)
 	}
+
 }
 
 // 根据数据库已有的表结构生成 model
--- pkg/tlog/tlog.go
+++ pkg/tlog/tlog.go
@@ -1,27 +1,44 @@
 package tlog
 
+import "C"
 import (
-	"fmt"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"os"
-	"strings"
 	"time"
 )
 
 var L zerolog.Logger
+var logFile *os.File
 
 func init() {
-	// todo 优化输出结构
-	r := zerolog.ConsoleWriter{
-		Out:        os.Stderr,
-		TimeFormat: time.RFC3339,
-		FormatLevel: func(i interface{}) string {
-			return strings.ToUpper(fmt.Sprintf("[%s]", i))
-		},
-		FormatMessage: func(i interface{}) string {
-			return fmt.Sprintf("%s", i)
-		},
+	// 将日志写入本地文件和标准错误输出
+	f, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
 	}
+	logFile = f
+	r := zerolog.MultiLevelWriter(logFile, os.Stderr)
 
-	L = zerolog.New(r).With().Timestamp().Logger()
+	zerolog.TimeFieldFormat = time.RFC3339
+	L = zerolog.New(r).With().Timestamp().Caller().Logger()
+	log.Logger = L
+
+	//// todo 优化输出结构
+	//r := zerolog.ConsoleWriter{
+	//	Out:        os.Stderr,
+	//	TimeFormat: time.RFC3339,
+	//	FormatLevel: func(i interface{}) string {
+	//		return strings.ToUpper(fmt.Sprintf("[%s]", i))
+	//	},
+	//	FormatMessage: func(i interface{}) string {
+	//		return fmt.Sprintf("%s", i)
+	//	},
+	//}
+
+	//L = zerolog.New(r).With().Timestamp().Logger()
+}
+
+func CloseLogFile() error {
+	return logFile.Close()
 }
